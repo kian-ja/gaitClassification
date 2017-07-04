f0cab77aeab6abcc7febcc38e90413fd59d6e2f7
Kian <seyed.jalaleddini@mail.mcgill.ca>
2017-07-03 18:06:27 -0700

    adding classificationFeature

---
+++ classificationFeature.m
@@ -0,0 +1,14 @@
+classdef classificationFeature
+	properties
+        
+	end
+	methods
+%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%new function%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%
+		function classFeatureObj = classificationFeature()
+			
+    	end
+
+	end	
+end
