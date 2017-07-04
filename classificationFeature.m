0b8a132a15fcea8ab92e957b8f4bfb1b3e539548
Kian <seyed.jalaleddini@mail.mcgill.ca>
2017-07-03 18:51:45 -0700

    check input type is ok

--- classificationFeature.m
+++ classificationFeature.m
@@ -1,13 +1,14 @@
 classdef classificationFeature
 	properties
-        feature_SMV_Power = 0;
-        feature_SMV_HighFreqPower = 0;
-        featureAccelXPower = 1;
-        featureAccelY_Power = 1;
-        featureAccelZPower = 0;
-        featureAccelXHighFreqPower = 0;
-        featureAccelYHighFreqPower = 0;
-        featureAccelZHighFreqPower = 0;
+        flagFeature_SMV_Power = 0;
+        flagFeature_SMV_HighFreqPower = 0;
+        flagFeatureAccelXPower = 1;
+        flagFeatureAccelY_Power = 1;
+        flagFeatureAccelZPower = 0;
+        flagFeatureAccelXHighFreqPower = 0;
+        flagFeatureAccelYHighFreqPower = 0;
+        flagFeatureAccelZHighFreqPower = 0;
+        feature = [];
 	end
 	methods
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -17,12 +18,44 @@ classdef classificationFeature
 			if (nargin < 1)
             	
             else
-                if numeric(data)
+                classFeatureObj = createFeature(classFeatureObj,data);
+            end
+        end
+        function classFeatureObj = createFeature(classFeatureObj,data)
+            if numeric(data)
+                if (size(data,2) > size(data,1))
+                    data = data';
+                end
+                if (size(data,2) == 3)
+                    data = 1;
                 else
                     warning('Input not supported')
                 end
+            elseif (class(data) == 'singleExperiment')
+                data = 1;
+            else
+                    warning('Input not supported')
             end
-    	end
-
+                
+            feature = [];
+            
+            if (classFeatureObj.flagFeature_SMV_Power)
+                feature = [feature; 
+            end
+            if (classFeatureObj.flagFeature_SMV_HighFreqPower)
+            end
+            if (classFeatureObj.flagFeatureAccelXPower)
+            end
+            if (classFeatureObj.flagFeatureAccelY_Power)
+            end
+            if (classFeatureObj.flagFeatureAccelZPower)
+            end
+            if (classFeatureObj.flagFeatureAccelXHighFreqPower)
+            end
+            if (classFeatureObj.flagFeatureAccelYHighFreqPower)
+            end
+            if (classFeatureObj.flagFeatureAccelZHighFreqPower)
+            end
+        end
 	end	
 end
