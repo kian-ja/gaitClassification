c5f6262d8fd27ac1722b1706d07e708489ec6c02
Kian <seyed.jalaleddini@mail.mcgill.ca>
2017-07-03 18:26:05 -0700

    defining member variable of the class classificationFeature

--- classificationFeature.m
+++ classificationFeature.m
@@ -1,13 +1,27 @@
 classdef classificationFeature
 	properties
-        
+        feature_SMV_Power = 0;
+        feature_SMV_HighFreqPower = 0;
+        featureAccelXPower = 1;
+        featureAccelY_Power = 1;
+        featureAccelZPower = 0;
+        featureAccelXHighFreqPower = 0;
+        featureAccelYHighFreqPower = 0;
+        featureAccelZHighFreqPower = 0;
 	end
 	methods
 %%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%new function%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%
-		function classFeatureObj = classificationFeature()
-			
+		function classFeatureObj = classificationFeature(data)
+			if (nargin < 1)
+            	
+            else
+                if numeric(data)
+                else
+                    warning('Input not supported')
+                end
+            end
     	end
 
 	end	
